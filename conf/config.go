c20584e27c28a74a97d5f4beacb1675bf18cb447
hongren chen <hongrenchen@hongrendeMacBook-Pro.local>
2023-02-20 21:36:29 +0800

    2.20

--- conf/config.go
+++ conf/config.go
@@ -8,9 +8,9 @@ const (
 // 服务配置
 const (
 	PORT              = "7777"
-	PrometheusPort    = "9200"    // 暴露给Prometheus的端口
-	ChunkMaxSize      = 100000000 // Chunk最大为100MB
-	HeartbeatInternal = 2         // 心跳间隔为2s
+	PrometheusPort    = "9200"   // 暴露给Prometheus的端口
+	ChunkMaxSize      = 10000000 // Chunk最大为10MB
+	HeartbeatInternal = 2        // 心跳间隔为2s
 )
 
 // MongoDB配置
